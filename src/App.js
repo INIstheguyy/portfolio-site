d1c5ca94c27a3ad51fd9d67f26ba724adfd0e9aa
Komoini <komolafeinioluwa9@gmail.com>
2023-05-03 22:21:00 +0100

    further commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styles from "./style";
+import { Navbar,Stats,Business,Billing,Testimonials,Clients,CTA,Footer,Hero} from "./components";
 function App() {
   return (
-    <div className="">
-      
+    <div className=" bg-primary w-full overflow-hidden">
+          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Navbar/>
+            </div>
+          </div>
+          <div className={`${styles.paddingX}`} >
+            <div className={`${styles.boxWidth}`}>
+              <Hero/>
+            </div>
+          </div>
+          <div className={`${styles.paddingX} ${styles.paddingX}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Stats/>
+              <Business/>
+              <Billing/>
+              <Testimonials/>
+              <Clients/>
+              <CTA/>
+              <Footer/>
+            </div>
+          </div>
     </div>
   );
 }
